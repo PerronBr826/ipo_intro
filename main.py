4df8692c818c0d1f5c43710e0c5787c9313de7d9
PerronBr826 <PerronBr826@northwested.org>
2024-09-18 10:48:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 # 9/18/2024
 # IPO Intro
 
+num1 = float(input("Input a number: (Example: 33)\n"))
+num2 = float(input("Input another number: (Example: 12)\n"))
+
+total = num1 + num2
+
+print(f"First number: {num1}")
+print(f"Second number: {num2}\n")
+print(f"The sum of both numbers is {total}.")
+
